5911ef9afd1c976803810fc87cc451347db9e764
Your Name <khadijadial07@gmail.com>
2023-02-20 23:54:01 -0800

    edited html 2

--- First Chart.R
+++ First Chart.R
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-install.packages("tidyverse", repos = "http://cran.us.r-project.org")
->>>>>>> 7bfec2bfb207ee22233786c6c6de8b8f84a0196f
 library("dplyr")
 library("ggplot2")
 
@@ -10,7 +6,6 @@ Mental_Health_Dataset <- read.csv("https://raw.githubusercontent.com/info-201a-w
 ggplot(data = Mental_Health_Dataset, aes(x = Relationships, y = SWLS_1,SWLS_2,SWLS_3,SWLS_4,SWLS_5,SWLS_G)) +
   geom_col(color = "blue", shape = 10, size = 1) +
   labs(x = "Relationship Status", y = "Life Satisfaction", title = "Relationship between Relationship Status and Life Satisfaction During the 4th Wave of the Pandemic")
-
 #This chart depicts the relationship between relationship status and self-prescribed overall life satisfaction on a ranking from 1-10 with 1 being the lowest score of overall life satisfaction and 10 being the highest. 
 #The Y-Axis displays the total values based on the survey samples collected(A measure of 1500 means that a sum score of 1500 occurred for that category based on the rating responses given).
 #From this visual representation, we can see that there is a correlation between relationship status and life satisfaction. Individuals currently in a couple relationship rated their life satisfaction much higher than those that were single and were interviewed.
