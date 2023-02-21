183bafe19b2ee1abfc0fe0f1f36868ed04b74eb1
Sadeem Alselmi <123619992+sadeemal@users.noreply.github.com>
2023-02-20 19:57:17 -0800

    table finished

---
+++ Table_of_summary_information.R
@@ -0,0 +1,40 @@
+#Sadeem Alselmi 
+#Table of Summmary information
+
+# Install packages 
+install.packages("readxl")
+install.packages("dplyr")
+
+library(readxl)
+library(dplyr)
+
+
+# Load the data 
+
+df <- read_excel("/Users/stlp//desktop/Dataset.xlsx")
+colnames(df)
+
+# Group by gender
+summary_table <- df %>% 
+  group_by(Gender) %>% 
+  summarize(
+    num_students_assessed = n(),
+    num_features = ncol(.),
+    percent_vaccinated = round(sum(Vaccination == "Yes") / num_students_assessed * 100, 2),
+    percent_covid_symptoms = round(sum(PCL_G == "Yes") / num_students_assessed * 100, 2),
+    percent_anxiety = round(sum(GAD_G == "Yes") / num_students_assessed * 100, 2),
+    percent_depression = round(sum(PHQ_G == "Yes") / num_students_assessed * 100, 2),
+    percent_anx_dep = round(sum(GAD_G == "Yes" & PHQ_G == "Yes") / num_students_assessed * 100, 2),
+    avg_hours_of_sleep = round(mean(SWLS_G, na.rm = TRUE), 2),
+    percent_supported = round(sum(FOCV == "Yes") / num_students_assessed * 100, 2)
+  ) %>% 
+  arrange(desc(num_students_assessed))
+
+# Display the summary table
+summary_table
+
+
+
+# Select a few columns and display the result
+select(summary_table, Gender, num_students_assessed, percent_vaccinated, avg_hours_of_sleep)
+
---
+++ summary_information.R
@@ -0,0 +1,41 @@
+#Sadeem Alselmi 
+#Summmary information
+
+# Install packages 
+install.packages("readxl")
+install.packages("dplyr")
+
+library(readxl)
+library(dplyr)
+
+
+# Load the data from excel file
+
+df <- read_excel("/Users/stlp//desktop/Dataset.xlsx")
+colnames(df)
+# Group by vaccination status and calculate summary statistics
+# Group by vaccination status and calculate summary statistics
+summary_df <- df %>%
+  group_by(Vaccination) %>%
+  summarise(
+    count = n(),
+    avg_age = round(mean(Age), 1),
+    avg_wellbeing = round(mean(SWLS_G), 2)
+  ) %>%
+  select(Vaccination, count, avg_age, avg_wellbeing) %>%
+  arrange(Vaccination, avg_age)
+
+
+# Display summary information
+cat("Summary Information\n")
+cat("------------------\n\n")
+cat("Dataset Description:\n")
+cat("The dataset is from Mendeley data: \"Mental health and well-being of university students during the fourth wave of the COVID-19 pandemic\". The website link is https://data.mendeley.com/datasets/bvcwx4tfjm. The data was collected from university students in Poland through an online survey. It includes information on the students' demographic and identity, their mental and physical health during the fourth wave of the COVID-19 pandemic, and their vaccination status. There are 706 observations (students) and 78 features (columns).\n\n")
+cat("Potential Biases:\n")
+cat("The data might be skewed in favor of or against vaccination. In addition, the data does not address the students' income or financial status, living conditions, or views on vaccinations.\n\n")
+cat("Summary Statistics by Vaccination Status:\n")
+cat("========================================\n")
+cat("\n")
+cat("The table below shows the number of students, their average age, and their average wellbeing score, grouped by vaccination status. The values are rounded to one decimal place (for age) and two decimal places (for wellbeing score).\n")
+cat("\n")
+cat(summary_df %>% knitr::kable(format = "markdown"))
